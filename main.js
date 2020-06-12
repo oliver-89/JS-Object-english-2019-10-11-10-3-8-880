d7dd66e0ca0cb2a844cece2367a3968fdb5b2004
oliver-89 <wjr980729@163.com>
2020-06-12 20:35:29 +0800

    finished

---
+++ main.js
@@ -0,0 +1,15 @@
+var user = {};
+user.name = 'John';
+user.surname = 'Mike';
+user.name = 'Peter';
+delete user.name
+
+var fruit = {
+	apple: 20,
+	pear: 20,
+	peach: 10
+};
+
+var num = fruit.apple + fruit.pear + fruit.peach;
+
+console.log(num)
